f865b755e22619be27a9c26a14016e3c6abaf331
Yuki Zhang <110796546+yuki172-zhang@users.noreply.github.com>
2025-02-07 15:53:41 -0800

    remove all

--- src/routes/feeds/utils.js
+++
@@ -1,3 +0,0 @@
-export const getIsValidJudgmentID = ({ judgmentID }) => {
-  return judgmentID === "allow" || judgmentID === "deny";
-};
--- src/routes/routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import feedsController from "./feeds/controller.js";
-
-const routes = Router().use(feedsController);
-
-export default routes;
